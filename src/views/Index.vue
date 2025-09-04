a1dd8a9d904985e417c7f1d7273ddda3d9361068
gyupp <kkb75281@gmail.com>
2025-09-04 15:40:41 +0900

    fix: index.vue 수정

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,17 +1,23 @@
 <template>
-	<main>
-		<h1>Index</h1>
-		<p>
-			This is a HTML template for authentication features using <a href='https://www.skapi.com'>Skapi</a>.
-			<br>
-			No CSS is used in this template. So don't let the design fool you.
-			<br>
-			This template packs full solid authentication features you can use in your HTML application.
-		</p>
-		<router-link to="/login">Login</router-link>
-		|
-		<router-link to="/signup">Sign-Up</router-link>
-	</main>
+    <main>
+        <h1>Skapi Vue Starter Template</h1>
+        <p>This is a Vue.js starter template integrated with Skapi.</p>
+        <p>It includes basic features for your Vue application:</p>
+        <ul>
+            <li>Signup</li>
+            <li>Signup email verification</li>
+            <li>Login</li>
+        </ul>
+
+        <h2>Important!</h2>
+        <p>Replace the SERVICE_ID and OWNER_ID values in src/main.js with your own service information.</p>
+        <p>You can get your own service ID from <a href='https://www.skapi.com'>Skapi</a>.</p>
+
+        <p style="font-weight: bold;margin-top: 24px;">Login or Sign-up below:</p>
+        <router-link to="/login">Login</router-link>
+        |
+        <router-link to="/signup">Sign-Up</router-link>
+    </main>
 </template>
 
 <script setup>
@@ -22,11 +28,11 @@ import { skapi } from '../main.js';
 const router = useRouter();
 
 onMounted(() => {
-	/*
+    /*
         Check if the user is logged in.
         If the user is logged in, redirect the user to welcome page.
     */
-	skapi.getProfile().then(user => {
+    skapi.getProfile().then(user => {
         if (user) {
             router.push('/welcome');
         }
